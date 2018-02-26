aa6027def535a677778e4d2fe0b8d7349dc2e33c
wofstech <gabrielabuka3@gmail.com>
2018-02-26 08:00:32 -0700

    'added allowed host'

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=f=jhk!ixamcx(q-(%zic=tle-z42@o6v--f7bda)d8sg@+0$0'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://vast-basin-21642.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
